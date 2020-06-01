f7674393b3e019804d59e18c0aa8d8f54450861d
gfu0311 <32159536+gfu0311@users.noreply.github.com>
2020-06-01 18:44:53 +0800

    office06/01

--- test.py
+++ test.py
@@ -51,3 +51,14 @@
 #             print(i)
 #             return(i)
 # test()
+list=[]
+for i in range(10):
+    list.append([])
+    for j in range(10):
+        list[i].append('-')
+
+
+for i in range(10):
+    for j in range(10):
+        print(list[i][j]+' ',end="")
+    print()
